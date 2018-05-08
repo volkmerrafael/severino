833be5c5b1968d584bb5d6e1004dec98022bf5af
volkmerrafael <rafael.volkmer1@gmail.com>
2018-05-08 19:57:21 -0300

    Modelo

---
+++ angular/src/app/model/ponto.ts
@@ -0,0 +1,19 @@
+import { Usuario } from "./usuario";
+
+export class Ponto {
+
+  id: number;
+  data: string;
+  diasemana: string;
+  jornada: string;
+  entrada1: string;
+  entrada2: string;
+  entrada3: string;
+  entrada4: string;
+  saida1: string;
+  saida2: string;
+  saida3: string;
+  saida4: string;
+  usuario: Usuario;
+
+}
