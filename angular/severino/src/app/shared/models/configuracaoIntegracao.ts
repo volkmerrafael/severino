e04e87c2a3dacb856596f23bd4638dfbf39ecafd
Rafael Volkmer <rafael.volkmer1@gmail.com>
2018-07-10 19:18:21 -0300

    Update configuracaoIntegracao.ts

--- angular/severino/src/app/shared/models/configuracaoIntegracao.ts
+++ angular/severino/src/app/shared/models/configuracaoIntegracao.ts
@@ -3,4 +3,12 @@ export class ConfiguracaoIntegracao {
     id: number;
     integra_jira: boolean;
     integra_slack: boolean;
+    email_notificacao: string;
+    email_host: string;
+    email_smtp_port:number;
+    email_senha: string;
+    apkey_google: string;
+    latitude: string;
+    longitude:string;
+    
 }
